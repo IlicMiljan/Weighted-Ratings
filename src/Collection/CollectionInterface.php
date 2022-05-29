17617ddc168ca2c5efa90bb86d0cf628c0de7017
Miljan Ilić <hello@miljanilic.com>
2022-05-29 14:29:53 +0200

    Create Collection
    Create RatingsCountCollection.php that implements CollectionInterface.php
    
    RatingsCountCollection.php represetns strongly-typed collection of Integers.

---
+++ src/Collection/CollectionInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace IlicMiljan\WeightedRatings\Collection;
+
+use IlicMiljan\WeightedRatings\Exception\InvalidTypeException;
+
+interface CollectionInterface
+{
+    /**
+     * Add element to Collection.
+     *
+     * @param $rating
+     * @return mixed
+     * @throws InvalidTypeException
+     */
+    public function add($rating);
+
+    /**
+     * Get All elements in Collection.
+     *
+     * @return array
+     */
+    public function all(): array;
+
+    /**
+     * Get count of elements in Collection.
+     *
+     * @return int
+     */
+    public function count(): int;
+
+    /**
+     * Returns TRUE when Collection is empty.
+     *
+     * @return bool
+     */
+    public function isEmpty(): bool;
+}
---
+++ src/Collection/RatingsCountCollection.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace IlicMiljan\WeightedRatings\Collection;
+
+use IlicMiljan\WeightedRatings\Exception\InvalidTypeException;
+
+class RatingsCountCollection implements CollectionInterface
+{
+    /** @var int[] $list */
+    private array $list = [];
+
+    /**
+     * @param mixed ...$ratings
+     * @throws InvalidTypeException
+     */
+    public function __construct(...$ratings)
+    {
+        $this->validateElement(...$ratings);
+        $this->list = $ratings;
+    }
+
+    /**
+     * @inheritDoc
+     * @param int $rating
+     */
+    public function add($rating): void
+    {
+        $this->validateElement($rating);
+        $this->list[] = $rating;
+    }
+
+    /**
+     * @inheritDoc
+     * @return int[]
+     */
+    public function all(): array
+    {
+        return $this->list;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function count(): int
+    {
+        return count($this->list);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isEmpty(): bool
+    {
+        return count($this->all()) === 0;
+    }
+
+    /**
+     * @param mixed ...$elements
+     * @return void
+     * @throws InvalidTypeException
+     */
+    private function validateElement(...$elements): void {
+        foreach ($elements as $element) {
+            if (!is_int($element)) {
+                throw new InvalidTypeException("Collection element must be of the type int, " . gettype($element) . " given.");
+            }
+        }
+    }
+}
