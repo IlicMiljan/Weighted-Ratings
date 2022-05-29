b24413b11ae252a157aa2e75ca6db83fb199cecf
Miljan Ilić <hello@miljanilic.com>
2022-05-29 17:43:03 +0200

    Create RatingWeightCalculator.php

---
+++ src/RatingWeightCalculator.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace IlicMiljan\WeightedRatings;
+
+use IlicMiljan\WeightedRatings\Collection\CollectionInterface;
+use IlicMiljan\WeightedRatings\Collection\RatingsCountCollection;
+use IlicMiljan\WeightedRatings\Config\RatingWeightConfig;
+use IlicMiljan\WeightedRatings\Exception\InvalidConfigurationException;
+use IlicMiljan\WeightedRatings\Exception\InvalidFormulaException;
+use IlicMiljan\WeightedRatings\Exception\InvalidTypeException;
+use IlicMiljan\WeightedRatings\Formula\BayesianApproximation;
+use IlicMiljan\WeightedRatings\Formula\FormulaInterface;
+use IlicMiljan\WeightedRatings\Formula\WilsonLowerBound;
+
+class RatingWeightCalculator
+{
+    public const RATING_WEIGHT_FORMULA_WILSON_LOWER_BOUND = 'wilson_lower_bound';
+    public const RATING_WEIGHT_FORMULA_BAYESIAN_APPROXIMATION = 'bayesian_approximation';
+
+    private RatingWeightConfig $ratingWeightConfig;
+
+    public function __construct(
+        ?RatingWeightConfig $ratingWeightConfig = null
+    ) {
+        $this->ratingWeightConfig = $ratingWeightConfig ?? new RatingWeightConfig();
+    }
+
+    /**
+     * @throws InvalidConfigurationException
+     */
+    public function formula(string $formula): self
+    {
+        if ($this->ratingWeightConfig->getFormula() !== null) {
+            throw new InvalidConfigurationException("Cannot change formula once it is configured.");
+        }
+
+        $this->ratingWeightConfig->formula($formula);
+
+        return $this;
+    }
+
+    /**
+     * @throws InvalidTypeException
+     * @throws Exception\EmptyCollectionException
+     * @throws InvalidFormulaException
+     * @throws InvalidConfigurationException
+     */
+    public function calculateWeight(array $ratingsCountPerStar): float {
+        $ratingsCountCollection = new RatingsCountCollection(...$ratingsCountPerStar);
+
+        $formula = $this->loadFormula($ratingsCountCollection);
+
+        return $formula->calculateWeight();
+    }
+
+    /**
+     * @throws Exception\EmptyCollectionException
+     * @throws InvalidFormulaException
+     * @throws InvalidConfigurationException
+     */
+    private function loadFormula(CollectionInterface $collection): FormulaInterface
+    {
+        if ($this->ratingWeightConfig->getFormula() === null) {
+            throw new InvalidConfigurationException("Formula is not configured.");
+        }
+
+        if ($this->ratingWeightConfig->getFormula() === self::RATING_WEIGHT_FORMULA_WILSON_LOWER_BOUND) {
+            return new WilsonLowerBound(
+                $collection,
+                $this->ratingWeightConfig
+            );
+        }
+
+        if ($this->ratingWeightConfig->getFormula() === self::RATING_WEIGHT_FORMULA_BAYESIAN_APPROXIMATION) {
+            return new BayesianApproximation(
+                $collection,
+                $this->ratingWeightConfig
+            );
+        }
+
+        throw new InvalidFormulaException();
+    }
+}
